8f2b03e167b5993a0946fe52f3fc5c879bebc5e9
Prasant Paudel <50891775+Prasant7878@users.noreply.github.com>
2020-07-12 01:06:29 +0545

    Add files via upload

--- image_compressor.py
+++ image_compressor.py
@@ -1,18 +1,18 @@
 from PIL import Image
+from pathlib import Path
 
-def compress(image_path, compression_percent):
-    # Open image
-    img = Image.open(image_path)
-    print(img.size)  # (5760, 3240)
-    x = img.size[0]*compression_percent/100
-    y = img.size[1]*compression_percent/100
-    img = img.resize((int(x),int(y)), Image.ANTIALIAS)
-    img.save(image_path, optimize=True, quality=95)
-    img.close()
 
-if __name__ == '__main__':
-    image_path = input('Image > ')
-    percent = input('Compression Percentage > ') 
-    compress(image_path, percent)
+def compress(image_path, minimum_original_size, compression_percent):
+    # get image size
+    size = Path(image_path).stat().st_size
+    if size >= minimum_original_size:
+        # Open image
+        img = Image.open(image_path)
+        x = img.size[0]*compression_percent/100
+        y = img.size[1]*compression_percent/100
+        img = img.resize((int(x),int(y)), Image.ANTIALIAS)
+        img.save(image_path, optimize=True, quality=95)
+        img.close()
+
 
 

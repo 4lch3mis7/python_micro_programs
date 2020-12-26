7fd299ba7418ab4ddded8481048841acfab6e3ae
Prasant Paudel <50891775+Prasant7878@users.noreply.github.com>
2020-12-26 05:58:48 +0300

    Update image_compressor.py
    Changed to class

--- image_compressor.py
+++ image_compressor.py
@@ -2,17 +2,21 @@ from PIL import Image
 from pathlib import Path
 
 
-def compress(image_path, minimum_original_size, compression_percent):
-    # get image size
-    size = Path(image_path).stat().st_size
-    if size >= minimum_original_size:
-        # Open image
+class ImageCompressor:
+    def __init__(self):
+        pass
+
+    def percent_of_original(self, source_file, percentage, output_file):
+        """Compress Image in percentage of size of original image"""
         img = Image.open(image_path)
-        x = img.size[0]*compression_percent/100
-        y = img.size[1]*compression_percent/100
-        img = img.resize((int(x),int(y)), Image.ANTIALIAS)
-        img.save(image_path, optimize=True, quality=95)
+        x = img.size[0]*percentage/100
+        y = img.size[1]*percentage/100
+        img = img.resize((int(x), int(y)), Image.ANTIALIAS)
+        img.save(output_file, optimize=True, quality=95)
         img.close()
 
 
-
+if __name__ == "__main__":
+    image_path = '/home/prasant/Downloads/Montessori Photos/12-20-2020/IMG_9910.JPG'
+    x = ImageCompressor()
+    x.percent_of_original(image_path, 10, 'compressed.JPG')
